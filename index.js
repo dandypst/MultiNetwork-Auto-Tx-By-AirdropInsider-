eaa00ec16645194fd80907fce303ed042c08ac18
dandypst <131114397+dandypst@users.noreply.github.com>
2025-02-24 18:02:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const WALLET_FILE = 'wallets.txt';
 const PK_FILE = 'pk.txt';
 const PROXY_FILE = 'proxies.txt';
 const FAUCET_API = networks.somnia.faucetApi;
+const FAUCET_API = networks.Humanity.faucetApi;
 
 const rl = readline.createInterface({
     input: process.stdin,
